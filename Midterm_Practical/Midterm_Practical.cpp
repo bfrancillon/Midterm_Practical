8bd6b1204632d4d4e512155fb3612679998752cd
Brianca Francillon <francillonbrianca15@gmail.com>
2022-07-15 21:07:14 -0400

    Complete

--- Midterm_Practical/Midterm_Practical.cpp
+++ Midterm_Practical/Midterm_Practical.cpp
@@ -8,104 +8,95 @@ int main()
 {
 	Questions question;
 	bool game = true;
-	int answer; 
-	unsigned short correctanswer = 0;
+	int answer;
 	const unsigned short count = 5;
-
-	while (game == true)
+	system("cls");
+	for (int i = 0; i < count; i++)
 	{
-		system("cls");
-		for (int i = 0; i < count; i++)
+
+		question.GetQuestionNumber1();
+		std::cin >> answer;
+		if (answer == 1)
 		{
-
-
-			int randQuestion = rand() % 6;
-
-			switch (randQuestion)
-			{
-			case 1:
-			{
-				question.GetQuestionNumber1();
-				std::cin >> answer;
-				if (answer == 1)
-				{
-					std::cout << "Correct!!!!\n";
-				}
-				else
-				{
-					std::cout << "InCorrect\n";
-				}
-
-				system("pause");
-
-				break;
-			}
-			case 2:
-			{
-				question.GetQuestionNumber2();
-				std::cin >> answer;
-				if (answer == 3)
-				{
-					std::cout << "Correct!!!!\n";
-				}
-				else if (answer == '1' || answer == '2' || answer == '4')
-				{
-					std::cout << "InCorrect\n";
-				}
-
-				break;
-			}
-			case 3:
-			{
-				question.GetQuestionNumber3();
-				std::cin >> answer;
-				if (answer == 4)
-				{
-					std::cout << "Correct!!!!\n";
-				}
-				else 
-				{
-					std::cout << "InCorrect\n";
-				}
-
-				break;
-			}
-			case 4:
-			{
-				question.GetQuestionNumber4();
-				std::cin >> answer;
-				if (answer == 1)
-				{
-					std::cout << "Correct!!!!\n";
-				}
-				else 
-				{
-					std::cout << "InCorrect\n";
-				}
-
-				break;
-			}
-			case 5:
-			{
-				question.GetQuestionNumber5();
-				std::cin >> answer;
-				if (answer == 2)
-				{
-					std::cout << "Correct!!!!\n";
-				}
-				else
-				{
-					std::cout << "InCorrect\n";
-				}
-
-				break;
-			}
-			break;
-
-			}
-			
+			std::cout << "Correct!!!!\n";
+			i++;
 
 		}
+		else
+		{
+			std::cout << "InCorrect\n";
+		}
+
+
+		question.GetQuestionNumber2();
+		std::cin >> answer;
+		if (answer == 3)
+		{
+			std::cout << "Correct!!!!\n";
+			i++;
+
+		}
+		else
+		{
+			std::cout << "InCorrect\n";
+		}
+
+
+		question.GetQuestionNumber3();
+		std::cin >> answer;
+		if (answer == 4)
+		{
+			std::cout << "Correct!!!!\n";
+			i++;
+
+		}
+		else
+		{
+			std::cout << "InCorrect\n";
+		}
+
+
+		question.GetQuestionNumber4();
+		std::cin >> answer;
+		if (answer == 1)
+		{
+			std::cout << "Correct!!!!\n";
+			i++;
+		}
+		else
+		{
+			std::cout << "InCorrect\n";
+		}
+
+
+
+		question.GetQuestionNumber5();
+		std::cin >> answer;
+		if (answer == 2)
+		{
+			std::cout << "Correct!!!!\n";
+			i++;
+		}
+		else
+		{
+			std::cout << "InCorrect\n";
+		}
+
+		//break;
+		//system("pause");
+		//system("cls");
+
+
+		std::cout << "Your score was a " << (i / 5.0) * 100 << "%!" << " \nThat is a " << i << "/5"<<std::endl;
+		system("pause");
+		system("cls");
+
+
+		//std::cout << "You got " << count << "/" << answer << " answers correct.\n";
+
+
+
 	}
 
 }
+	
--- Midterm_Practical/Questions.cpp
+++ Midterm_Practical/Questions.cpp
@@ -9,28 +9,28 @@ void Questions::GetQuestionNumber1()
 
 void Questions::GetQuestionNumber2()
 {
-	QuestionNumber2 = "Which one of these character is not a friend of Harry Potter?\n ";
+	QuestionNumber2 = "\nWhich one of these character is not a friend of Harry Potter?\n ";
 	std::cout << QuestionNumber2;
 	std::cout << "1) Ron Weasley\n 2) Neville Longbottom\n 3) Draco Malfoy\n 4) Hermione Granger\n";
 }
 
 void Questions::GetQuestionNumber3()
 {
-	QuestionNumber3 = "Which animal does not appear on the Chinese zodiac? \n";
+	QuestionNumber3 = "\nWhich animal does not appear on the Chinese zodiac? \n";
 	std::cout << QuestionNumber3;
 	std::cout << "1) Dragon\n 2) Rabbit\n 3) Dog\n 4) Hummingbird\n";
 }
 
 void Questions::GetQuestionNumber4()
 {
-	QuestionNumber4 = "Which planet is the hottest? \n";
+	QuestionNumber4 = "\nWhich planet is the hottest? \n";
 	std::cout << QuestionNumber4;
 	std::cout << "1) Venus\n 2) Saturn\n 3) Mercury\n 4) Mars\n";
 }
 
 void Questions::GetQuestionNumber5()
 {
-	QuestionNumber5 = "In the Pirates of the Caribbean, what was Captain Jack Sparrow's Ship's name? \n";
+	QuestionNumber5 = "\nIn the Pirates of the Caribbean, what was Captain Jack Sparrow's Ship's name? \n";
 	std::cout << QuestionNumber5;
 	std::cout << "1) The Marauder\n 2) The Black Pearl\n 3) The Sparrow Pearl\n 4) The black Pyton\n";
 }
